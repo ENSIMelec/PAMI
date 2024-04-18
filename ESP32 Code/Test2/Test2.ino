23b25dbe8d4310b723f42641db27b5a3a809bc4c
Estebancott <estebancott@gmail.com>
2024-04-18 19:29:27 +0200

    Carte elec

--- ESP32 Code/Test2/Test2.ino
+++ ESP32 Code/Test2/Test2.ino
@@ -1,21 +1,21 @@
 #include "./MeEncoderNew.h"
 
-MeEncoderNew motor1(0x18, SLOT1);   //  motor at slot1
-MeEncoderNew motor2(0x18, SLOT2);   //  motor at slot2
+MeEncoderNew moteurGauche(0x18, SLOT1);   //  motor at slot1
+MeEncoderNew moteurDroit(0x18, SLOT2);   //  motor at slot2
 
 void setup()
 {
-  motor1.begin();
-  motor2.begin();
+  moteurGauche.begin();
+  moteurDroit.begin();
   Serial.begin(115200);
 }
 
 void loop()
 {
-  motor1.runSpeed(-150);
-  motor2.runSpeed(50);
+  moteurGauche.runSpeed(-150);
+  moteurDroit.runSpeed(50);
   delay(3000);
-  motor1.runSpeed(0);
-  motor2.runSpeed(0);
+  moteurGauche.runSpeed(0);
+  moteurDroit.runSpeed(0);
   delay(2000);
 }
